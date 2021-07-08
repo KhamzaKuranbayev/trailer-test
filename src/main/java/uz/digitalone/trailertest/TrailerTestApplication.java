fbc468eab7aebbc4863190c42145e128c9b9b2c7
Khorezmiy1991 <khamza@inbox.ru>
2021-07-08 12:22:33 +0500

    first commit

---
+++ src/main/java/uz/digitalone/trailertest/TrailerTestApplication.java
@@ -0,0 +1,13 @@
+package uz.digitalone.trailertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrailerTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/digitalone/trailertest/TrailerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.digitalone.trailertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrailerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
