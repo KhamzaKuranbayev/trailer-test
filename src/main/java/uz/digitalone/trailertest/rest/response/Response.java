176dc514ac0cd11dc68aac361cdaf056ecfba8b2
Khorezmiy1991 <khamza@inbox.ru>
2021-07-09 17:33:29 +0500

    added Response

--- src/main/java/uz/digitalone/trailertest/rest/response/Response.java
+++ src/main/java/uz/digitalone/trailertest/rest/response/Response.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -14,7 +16,17 @@ public class Response {
     private boolean success;
     private String message;
     private Object data;
+    private List<Object> dataList;
 
+    public Response(boolean success, String message, Object data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
 
-
+    public Response(boolean success, String message, List<Object> dataList) {
+        this.success = success;
+        this.message = message;
+        this.dataList = dataList;
+    }
 }
